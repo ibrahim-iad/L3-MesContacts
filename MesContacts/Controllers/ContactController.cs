96d9368df2b647bc3c0b687501d62553d9682173
Ibrahim Mohamed <ibroman79@gmail.com>
2023-05-16 17:10:57 +0300

    Ajoutez des fichiers projet.

---
+++ MesContacts/Controllers/ContactController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MesContacts.Models;
+
+namespace MesContacts.Controllers
+{
+    public class ContactController : Controller
+    {
+        private mescontactsEntities db = new mescontactsEntities();
+
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var contacts = db.contacts.Include(c => c.groupe);
+            return View(contacts.ToList());
+        }
+
+        // GET: Contact/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contact contact = db.contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // GET: Contact/Create
+        public ActionResult Create()
+        {
+            ViewBag.groupe_id = new SelectList(db.groupes, "id", "nom");
+            return View();
+        }
+
+        // POST: Contact/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nom,telephone,email,groupe_id")] contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.groupe_id = new SelectList(db.groupes, "id", "nom", contact.groupe_id);
+            return View(contact);
+        }
+
+        // GET: Contact/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contact contact = db.contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.groupe_id = new SelectList(db.groupes, "id", "nom", contact.groupe_id);
+            return View(contact);
+        }
+
+        // POST: Contact/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nom,telephone,email,groupe_id")] contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.groupe_id = new SelectList(db.groupes, "id", "nom", contact.groupe_id);
+            return View(contact);
+        }
+
+        // GET: Contact/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contact contact = db.contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contact/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            contact contact = db.contacts.Find(id);
+            db.contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MesContacts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MesContacts.MvcApplication" Language="C#" %>
